968a72dd742c99c53fca8ae3c8a884c6452afdc2
liwei <henyouqian@gmail.com>
2014-02-07 00:53:41 +0800

    lw

--- html5/slider/cocos2d.js
+++ html5/slider/cocos2d.js
@@ -35,13 +35,12 @@
         loadExtension:false,
         renderMode:0,       //Choose of RenderMode: 0(default), 1(Canvas only), 2(WebGL only)
         tag:'gameCanvas', //the dom element to run cocos2d on
-        engineDir:'../cocos2d/',
-        //SingleEngineFile:'',
-        appFiles:[
-            'src/resource.js',
-            'src/sliderScene.js',
-            'src/zepto.min.js',
-        ]
+        //engineDir:'../cocos2d/',
+        SingleEngineFile:'slider.js'
+        // appFiles:[
+        //     'src/sliderScene.js',
+        //     'src/zepto.min.js',
+        // ]
     };
 
     if(!d.createElement('canvas').getContext){
