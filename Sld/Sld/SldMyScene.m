d418069610e652d8e6a3542431788d0d7686f169
liwei <henyouqian@gmail.com>
2014-04-06 15:00:13 +0800

    lw

--- Sld/Sld/SldMyScene.m
+++ Sld/Sld/SldMyScene.m
@@ -9,33 +9,31 @@
 #import "SldMyScene.h"
 #import "giflib/gif_lib.h"
 
+@interface TextureInfo : NSObject
+    @property SKTexture *texture;
+    @property Float32 duration;
+@end
+@implementation TextureInfo
+@end
+
+
 @interface SldMyScene()
-
-@property GifFileType *gifFile;
-@property NSUInteger currFrame;
-@property SKSpriteNode *sprite;
-@property char *buff;
-@property Float32 waitSec;
-
+    @property GifFileType *gifFile;
+    @property NSUInteger currFrame;
+    @property SKSpriteNode *sprite;
+    @property char *buff;
+    @property Float32 waitSec;
+    @property NSMutableArray *textures;
 @end
 
 @implementation SldMyScene
-
-//static int pwr2Size(int in) {
-//    int out = 2;
-//    while (1) {
-//        if (out >= in)
-//            return out;
-//        out *= 2;
-//    }
-//}
 
 -(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         self.currFrame = -1;
         int err = GIF_OK;
-        NSString *path = [[NSBundle mainBundle] pathForResource:@"img/x.gif" ofType:nil];
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"img/c.gif" ofType:nil];
         self.gifFile = DGifOpenFileName([path UTF8String], &err);
         err = DGifSlurp(self.gifFile);
         
@@ -44,7 +42,9 @@
         self.buff = malloc(bufLen);
         memset(self.buff, 0, bufLen);
         
+        //
         self.waitSec = 0;
+        self.textures = [NSMutableArray arrayWithCapacity:10];
         
         [self updateBuff];
     }
@@ -62,6 +62,13 @@
         self.currFrame = 0;
     }
     
+//    if (self.currFrame < [self.textures count]) {
+//        TextureInfo *texInfo = self.textures[self.currFrame];
+//        self.sprite.texture = texInfo.texture;
+//        self.waitSec += texInfo.duration;
+//        return;
+//    }
+    
     SavedImage *img = self.gifFile->SavedImages + self.currFrame;
     GifImageDesc *imgDesc = &img->ImageDesc;
     ColorMapObject *colorMap = imgDesc->ColorMap;
@@ -71,7 +78,7 @@
     }
     
     //ext code
-    double currFrameDuration = 0;
+    Float32 currFrameDuration = 0;
     bool hasTrans = false;
     unsigned char transIdx = 0;
     GraphicsControlBlock gcb;
@@ -88,42 +95,72 @@
             break;
         }
     }
-    self.waitSec += (Float32)currFrameDuration;
+    self.waitSec += currFrameDuration;
+    int disposalMode = gcb.DisposalMode;
     
     //read pixel
     int i = 0;
     GifWord gifWidth = self.gifFile->SWidth;
     GifWord gifHeight = self.gifFile->SHeight;
-    size_t colorSize = sizeof(GifByteType)*4;
     GifWord top = imgDesc->Top;
     GifWord height = imgDesc->Height;
     GifWord left = imgDesc->Left;
     GifWord width = imgDesc->Width;
     CGSize size = CGSizeMake(gifWidth, gifHeight);
-    for (GifWord y = top; y < top + height; ++y) {
-        for (GifWord x = left; x < left + width; ++x) {
-            unsigned char colorIdx = imgBuf[i];
-            GifWord revY = gifHeight - y - 1;
-            
-            if (hasTrans && colorIdx == transIdx) {
-                if (gcb.DisposalMode == DISPOSE_BACKGROUND) {
-                    GifColorType *bgColor = &(colorMap->Colors[_gifFile->SBackGroundColor]);
-                    GifByteType c[4] = {bgColor->Red, bgColor->Green, bgColor->Blue, 0xff};
-                    memcpy(self.buff+(gifWidth*revY+x)*4, c, colorSize);
+    GifWord yMax = top + height;
+    GifWord xMax = left + width;
+    char *buf = self.buff;
+    if (hasTrans) {
+        for (int y = top; y < yMax; ++y) {
+            int revY = gifHeight - y - 1;
+            int offset = gifWidth*revY;
+            for (GifWord x = left; x < xMax; ++x) {
+                unsigned char colorIdx = imgBuf[i];
+                if (colorIdx == transIdx) {
+                    if (disposalMode == DISPOSE_BACKGROUND) {
+                        GifColorType *bgColor = &(colorMap->Colors[_gifFile->SBackGroundColor]);
+                        //GifByteType c[4] = {bgColor->Red, bgColor->Green, bgColor->Blue, 0xff};
+                        //memcpy(self.buff+(offset+x)*4, c, colorSize);
+                        char *p = self.buff+((offset+x)<<2);
+                        p[0] = bgColor->Red;
+                        p[1] = bgColor->Green;
+                        p[2] = bgColor->Blue;
+                        p[3] = 0xff;
+                    }
+                    ++i;
+                    continue;
                 }
+                
+                GifColorType *color = &(colorMap->Colors[colorIdx]);
+//                GifByteType c[4] = {color->Red, color->Green, color->Blue, 0xff};
+//                memcpy(self.buff+(offset+x)*4, c, colorSize);
+                char *p = buf+((offset+x)<<2);
+                p[0] = color->Red;
+                p[1] = color->Green;
+                p[2] = color->Blue;
+                p[3] = 0xff;
                 ++i;
-                continue;
             }
-            
-            GifColorType *color = &(colorMap->Colors[imgBuf[i]]);
-            GifByteType c[4] = {color->Red, color->Green, color->Blue, 0xff};
-            memcpy(self.buff+(gifWidth*revY+x)*4, c, colorSize);
-            ++i;
+        }
+    } else {
+        for (GifWord y = top; y < yMax; ++y) {
+            GifWord revY = gifHeight - y - 1;
+            int offset = gifWidth*revY;
+            for (GifWord x = left; x < xMax; ++x) {
+                unsigned char colorIdx = imgBuf[i];
+                GifColorType *color = &(colorMap->Colors[colorIdx]);
+                char *p = buf+((offset+x)<<2);
+                p[0] = color->Red;
+                p[1] = color->Green;
+                p[2] = color->Blue;
+                p[3] = 0xff;
+                ++i;
+            }
         }
     }
     
-    NSData *buf = [NSData dataWithBytes:self.buff length:gifWidth*gifHeight*4];
-    SKTexture *texture = [SKTexture textureWithData:buf size:size];
+    NSData *data = [NSData dataWithBytes:self.buff length:gifWidth*gifHeight*4];
+    SKTexture *texture = [SKTexture textureWithData:data size:size];
     
 //    for (int i = 0; i < 10; ++i) {
 //        texture = [SKTexture textureWithRect:CGRectMake(0, 0, 1, 1) inTexture:texture];
@@ -139,11 +176,29 @@
     } else {
         self.sprite.texture = texture;
     }
+    
+//    TextureInfo *texInfo = [[TextureInfo alloc] init];
+//    texInfo.texture = self.sprite.texture;
+//    texInfo.duration = currFrameDuration;
+//    [self.textures addObject:texInfo];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     //[self updateBuff];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint ptPrev = [touch previousLocationInView:self.view];
+    CGPoint pt = [touch locationInView:self.view];
+    
+    __weak SKSpriteNode *sprite = self.sprite;
+    [sprite setPosition:CGPointMake(sprite.position.x+pt.x-ptPrev.x, sprite.position.y-pt.y+ptPrev.y)];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
 }
 
 -(void)update:(CFTimeInterval)currentTime {
